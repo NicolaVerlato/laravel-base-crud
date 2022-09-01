320f6e2b4b36b46daf83bfe8113866c0c0324a2c
NicolaVerlato <nicola.verlato00@gmail.com>
2022-09-01 14:41:03 +0200

    import bootstrap

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>I nostri fumetti</h1>
+   <div class="container">
+        <h1>I nostri fumetti</h1>
 
-    @foreach ($comics as $comic)
-        <div>
-            <h3>
-                {{ $comic->title }}
-            </h3>
+        @foreach ($comics as $comic)
             <div>
-                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
-                <p><i>{{ $comic->description }}</i></p>
+                <h3>
+                    {{ $comic->title }}
+                </h3>
+                <div>
+                    <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                    <p><i>{{ $comic->description }}</i></p>
+                </div>
+                <div>
+                    <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">Vai al dettaglio</a>
+                </div>
+                <div>
+                    <a href="{{ route('comics.edit', ['comic' => $comic->id]) }}">Modifica</a>
+                </div>
+                <div>
+                    <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Elimina" onClick="return confirm('Sei sicuro di volerlo eliminare?')">
+                    </form>
+                </div>
             </div>
-            <div>
-                <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">Vai al dettaglio</a>
-            </div>
-            <div>
-                <a href="{{ route('comics.edit', ['comic' => $comic->id]) }}">Modifica</a>
-            </div>
-            <div>
-                <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" value="Elimina" onClick="return confirm('Sei sicuro di volerlo eliminare?')">
-                </form>
-            </div>
-        </div>
-        <br>
-    @endforeach
+            <br>
+        @endforeach
+   </div>
 @endsection
